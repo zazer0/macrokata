0e9f9ef72868182e7fa9fd3b50d9a9623ec4dea9
zazer0 <zazer.unsw@gmail.com>
2025-11-12 14:18:13 +0000

    feat: solve 03!

--- exercises/03_literal_variables/main.rs
+++ exercises/03_literal_variables/main.rs
@@ -5,6 +5,15 @@ fn print_result(num: i32) {
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
 // TODO: create `math!()` macro.
+macro_rules! math {
+    ($var1:literal plus $var2:literal) => {
+        $var1 + $var2
+    };
+
+    (square $metavar:literal) => {
+        $metavar * $metavar
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
